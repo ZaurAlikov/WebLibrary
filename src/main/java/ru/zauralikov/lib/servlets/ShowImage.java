6af1b05e18c1d661eee647f216c913332175a954
Zaur Alikov <ochenmnogopisem@gmail.com>
2017-09-09 00:53:43 +0300

    show books

--- src/main/java/ru/zauralikov/lib/servlets/ShowImage.java
+++ src/main/java/ru/zauralikov/lib/servlets/ShowImage.java
@@ -1,44 +1,40 @@
 package ru.zauralikov.lib.servlets;
 
-import ru.zauralikov.lib.beans.Book;
-
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.util.ArrayList;
+import ru.zauralikov.lib.beans.Book;
 
 public class ShowImage extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("image/jpeg");
         OutputStream out = response.getOutputStream();
-
-        try{
+        try {
             int index = Integer.valueOf(request.getParameter("index"));
 
-            ArrayList<Book> list = (ArrayList<Book>) request.getSession(false).getAttribute("currentBookList");
+            ArrayList<Book> list = (ArrayList<Book>)request.getSession(false).getAttribute("currentBookList");
             Book book = list.get(index);
+            response.setContentLength(book.getImage().length);
             out.write(book.getImage());
-            out.flush();
         } finally {
+
             out.close();
         }
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doGet(request, response);
         processRequest(request, response);
 
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doPost(request, response);
         processRequest(request, response);
     }
 }
